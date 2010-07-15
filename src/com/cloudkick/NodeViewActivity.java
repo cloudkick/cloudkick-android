6112d96e2bcd91f2dd16237f121a5aba481529a7
Russell Haering <rusell@cloudkick.com>
2010-07-15 15:23:56 -0700

    Start filling in the node detail view.

--- src/com/cloudkick/NodeViewActivity.java
+++ src/com/cloudkick/NodeViewActivity.java
@@ -1,31 +1,30 @@
 package com.cloudkick;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class NodeViewActivity extends Activity {
 	private static final String TAG = "Node";
 	private Node node;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-	    super.onCreate(savedInstanceState);
-	    Bundle data = this.getIntent().getExtras();
-	    node = (Node) data.getSerializable("node");
-	    TextView view = new TextView(this);
-	    view.setText(node.getName());
-	    setContentView(view);
-	    /*
-		RelativeLayout nodeView;
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Bundle data = this.getIntent().getExtras();
+		node = (Node) data.getSerializable("node");
+
+		RelativeLayout nodeView;
 		String inflater = Context.LAYOUT_INFLATER_SERVICE;
-		LayoutInflater li = (LayoutInflater)getSystemService(inflater);
+		LayoutInflater li = (LayoutInflater) getSystemService(inflater);
 
 		nodeView = new RelativeLayout(this);
 		li.inflate(R.layout.node_view, nodeView, true);
-		
+
 		// Build the tag string
 		StringBuilder tagString = new StringBuilder();
 		if (node.getTags().length > 0) {
@@ -35,17 +34,19 @@ public class NodeViewActivity extends Activity {
 				tagString.append(node.getTags()[i]);
 			}
 		}
-		
+
 		// Set the background
 		nodeView.setBackgroundDrawable(new ColorDrawable(node.getColor()));
-		
+
 		// Set the name and tags
-		TextView nameText = (TextView)nodeView.findViewById(R.id.name);
-		TextView tagsText = (TextView)nodeView.findViewById(R.id.tags);
-		nameText.setText(node.name);
+		TextView nameText = (TextView) nodeView
+				.findViewById(R.id.node_detail_name);
+		TextView tagsText = (TextView) nodeView
+				.findViewById(R.id.node_detail_tags);
+		nameText.setText(node.getName());
 		tagsText.setText(tagString.toString());
 
-		this.setContentView(nodeView);
-		*/
+		setContentView(nodeView);
+		setTitle("Node: " + node.getName());
 	}
 }

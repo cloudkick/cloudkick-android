fa3a09259db7a6f1d8527b13ad04adeb221bd873
Russell Haering <rusell@cloudkick.com>
2010-07-15 21:09:25 -0700

    Improved settings reloading after modification

--- src/com/cloudkick/DashboardActivity.java
+++ src/com/cloudkick/DashboardActivity.java
@@ -1,12 +1,12 @@
 package com.cloudkick;
 
-import org.json.JSONException;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,32 +15,34 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.preference.PreferenceManager;
-import android.content.SharedPreferences;
 
 public class DashboardActivity extends Activity {
 	private static final String TAG = "DashboardActivity";
+	private static final int SETTINGS_ACTIVITY_ID = 0;
 	private CloudkickAPI api;
 	private ProgressDialog progress;
 	private ListView dashboard;
 	private NodesAdapter adapter;
 	private Node[] nodes = new Node[0];
 	private SharedPreferences prefs;
-	
-	private void refreshNodes()
-	{
+
+	private void refreshNodes() {
 	    progress = ProgressDialog.show(this, "", "Retrieving Nodes...", true);
 		new NodeUpdater().execute();
 	}
-	
+
+	private void reloadAPI() {
+	    api = new CloudkickAPI(prefs.getString("editKey", ""), prefs.getString("editSecret", ""));
+	    refreshNodes();
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 	    super.onCreate(savedInstanceState);
 	    PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 	    prefs = PreferenceManager.getDefaultSharedPreferences(this);
-	    api = new CloudkickAPI(prefs.getString("editKey", ""), prefs.getString("editSecret", ""));
-	    
+
 	    dashboard = new ListView(this);
 	    adapter = new NodesAdapter(this, R.layout.node_item, nodes);
 		dashboard.setAdapter(adapter);
@@ -50,21 +52,21 @@ public class DashboardActivity extends Activity {
 				data.putSerializable("node", nodes[position]);
 				Intent intent = new Intent(DashboardActivity.this, NodeViewActivity.class);
 				intent.putExtras(data);
-				startActivityForResult(intent, 0);
+				startActivity(intent);
 			}
 		});
 
 	    setContentView(dashboard);
-	    refreshNodes();
+	    reloadAPI();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.dashboard_menu, menu);
 	    return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    switch (item.getItemId()) {
@@ -73,18 +75,27 @@ public class DashboardActivity extends Activity {
 		        return true;
 		    case R.id.settings:
 		    	Intent settingsActivity = new Intent(getBaseContext(), Preferences.class);
-		    	startActivity(settingsActivity);
+		    	startActivityForResult(settingsActivity, SETTINGS_ACTIVITY_ID);
 		    	return true;
 		    default:
 		        return super.onOptionsItemSelected(item);
 	    }
 	}
-	
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == SETTINGS_ACTIVITY_ID) {
+			reloadAPI();
+		}
+	}
+
 	private class NodeUpdater extends AsyncTask<Void, Void, Node[]> {
+		@Override
 		protected Node[] doInBackground(Void...voids) {
 			return api.getNodes();
 		}
-		
+
+		@Override
 		protected void onPostExecute(Node[] retrieved_nodes) {
 			try {
 				Log.i(TAG, "Retrieved " + retrieved_nodes.length + " Nodes");
@@ -97,7 +108,7 @@ public class DashboardActivity extends Activity {
 			catch (Exception e) {
 				progress.dismiss();
 				Intent settingsActivity = new Intent(getBaseContext(), Preferences.class);
-		    	startActivity(settingsActivity);
+		    	startActivityForResult(settingsActivity, SETTINGS_ACTIVITY_ID);
 			}
 		}
 	}
